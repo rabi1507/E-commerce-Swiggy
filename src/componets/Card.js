e0cb531e06ca3891013061f358b77643b526af5b
Rabi Kumar <rabi.mkumar12@gmail.com>
2024-08-02 18:55:44 +0530

    wrapping up food ordering app

--- src/componets/Card.js
+++ src/componets/Card.js
@@ -11,7 +11,7 @@ const Card =()=>{
     return (
         <div className=" p-6 mt-20 max-w-sm mx-auto bg-white border border-gray-200 rounded-lg shadow-md justify-center h-screen">
             <h4 className=" text-center font-bold text-amber-700"> Card Item </h4>
-            <button className="bg-black rounded-lg text-white items-center m-4 shadow-rose-800" onClick={handleClick}> Clear Card </button>
+            <button className="p-2 m-2 px-6 mt-4 mx-20 bg-black rounded-lg text-white items-center shadow-rose-800" onClick={handleClick}> Clear Card </button>
             <ItemList itemList = {cardStore}/>
         </div>
     )
